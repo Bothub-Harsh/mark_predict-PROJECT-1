44809c0c3492cc41659140eda633f533a8b37d4f
Harsh Meshram <harshmeshram838@gmail.com>
2026-02-17 20:28:23 +0530

    add

---
+++ ai_module/predict.py
@@ -0,0 +1,11 @@
+import pickle
+import numpy as np
+
+# Load model
+with open("./models/model.pkl", "rb") as f:
+    model = pickle.load(f)
+
+hours = float(input("Enter study hours: "))
+prediction = model.predict(np.array([[hours]]))
+
+print(f"Predicted Marks: {prediction[0]}")
---
+++ ai_module/train.py
@@ -0,0 +1,19 @@
+import pandas as pd
+import pickle
+from sklearn.linear_model import LinearRegression
+
+# Load dataset
+df = pd.read_csv("./data/student_data.csv")
+
+X = df[["Hours"]]
+y = df["Marks"]
+
+# Train model
+model = LinearRegression()
+model.fit(X, y)
+
+# Save model
+with open("../models/model.pkl", "wb") as f:
+    pickle.dump(model, f)
+
+print("Model trained and saved successfully!")
